8c5c69cb9bf105108e067b6a8f47332567b46d78
WeeverAlex <awever4000@gmail.com>
2023-04-24 13:59:38 -0300

    for

---
+++ bin/repeaters.dart
@@ -0,0 +1,20 @@
+void main() {
+  //for
+  // for (int index = 0; index < 5; index++) {
+  //   print(index);
+  // }
+
+  // for in
+  // var list = [1, 2, 3, 4, 5];
+  // for (var item in list) {
+  //   print(item);
+  // }
+
+  // for each
+  var listB = [1, 2, 3, 4, 5];
+  // listB.forEach((element) {
+  //   print(element);
+  // });
+  
+  listB.forEach((item) => print(item));
+}
